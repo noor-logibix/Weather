fbd58d8835f07205a204a22cf2242002dae385a8
Noor <noor.offl@gmail.com>
2023-05-06 12:43:40 +0530

    Weather service implemented

--- app/src/main/java/com/example/weather/network/WeatherApiService.kt
+++ app/src/main/java/com/example/weather/network/WeatherApiService.kt
@@ -1,12 +1,13 @@
 package com.example.weather.network
 
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 private const val BASE_URL = "https://api.openweathermap.org/"
+
 const val API_KEY = "5f3219e74f1ae7d015c46e1e1d16acf3"
 
 private val retrofit = Retrofit.Builder()
@@ -16,9 +17,7 @@ private val retrofit = Retrofit.Builder()
 
 interface WeatherApiService {
     @GET("data/2.5/weather")
-    fun getForecast(@QueryMap options: Map<String, String>): WeatherDto
-    //@Query("appid=5f3219e74f1ae7d015c46e1e1d16acf3")
-    //fun getForecast(@Query("q") city: String): WeatherDto
+    fun getForecast(@QueryMap options: Map<String, String>): Call<WeatherDto>
 }
 
 object WeatherApi {
--- app/src/main/java/com/example/weather/today/TodayFragment.kt
+++ app/src/main/java/com/example/weather/today/TodayFragment.kt
@@ -9,10 +9,13 @@ import android.view.ViewGroup
 import com.example.weather.R
 import com.example.weather.network.API_KEY
 import com.example.weather.network.WeatherApi
-import java.lang.Exception
-import kotlin.math.log
+import com.example.weather.network.WeatherDto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class TodayFragment : Fragment() {
+    val TAG = "TodayFragment"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,12 +23,22 @@ class TodayFragment : Fragment() {
         val map = mutableMapOf<String, String>()
         map["q"] = "Malda"
         map["appid"] = API_KEY
-        try {
-            val result = WeatherApi.retrofitService.getForecast(map)
-            Log.d("TodayFragment", "onCreateView: $result")
-        } catch (e: Exception) {
-            Log.d("TodayFragment", "Exception: ${e.message}")
-        }
+
+        WeatherApi.retrofitService.getForecast(map).enqueue(object : Callback<WeatherDto> {
+            override fun onResponse(
+                call: Call<WeatherDto>,
+                response: Response<WeatherDto>
+            ) {
+                Log.d(TAG, "onResponse: ${response.body()}")
+
+
+            }
+
+            override fun onFailure(call: Call<WeatherDto>, t: Throwable) {
+                Log.d(TAG, "onFailure: ${t.message}")
+            }
+        })
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_today, container, false)
     }
